7ef60e29ebe92d2d4030e950b09c6b88030e5cac
K-Paks <paksyfr@gmail.com>
2022-04-27 11:53:54 +0200

    rm arch/

--- arch/preprocessing.py
+++
@@ -1,5 +0,0 @@
-import os
-from os.path import join
-
-DATA_PATH = join('data_capt', 'subtitles_processed')
-
--- arch/sentence_sim_model.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-from sentence_transformers import util
-
-with open('model.pkl', 'rb') as f:
-    model = pickle.load(f)
-
-util
