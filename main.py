60b1ababd2c3ecc5f13dfb4e01aec48693313b91
K-Paks <paksyfr@gmail.com>
2022-05-07 19:23:36 +0200

    name fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from src.model import NLPModel
+from src.model.nlp_model import NLPModel
 
 # header
 st.title("Healthy Gamer Search Engine")
@@ -20,14 +20,14 @@ if query:
         # show results
         col1, mid, col2 = st.columns([1, 3, 20])
         with col1:
-            st.image(f'https://i.ytimg.com/vi/{sugg.video_id}/default.jpg', width=100)
+            st.image(
+                f'https://i.ytimg.com/vi/{sugg.video_id}/default.jpg', width=100)
         with col2:
-            st.markdown(f'[{sugg.title}](https://www.youtube.com/watch?v={sugg.video_id})', unsafe_allow_html=True)
+            st.markdown(
+                f'[{sugg.title}](https://www.youtube.com/watch?v={sugg.video_id})', unsafe_allow_html=True)
 
             if sugg.name != 'None':
                 sugg.name = sugg.name.replace('- ', '')
                 st.markdown(
                     f'Relatable part: [{sugg.name}](https://youtu.be/{sugg.video_id}?t={sugg.time_raw}). '
                     f'Starts at {sugg.time_start}')
-
-
